363163b5715752cf59ec63ada6a019ab7d01dd3e
Carl J <carlj1881@yahoo.com>
2020-10-02 01:11:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,13 +6,14 @@ class CashRegister
   
   def initialize(discount = 0)
     @total = 0
-    @discount = discount * 100
+    @discount = 20
   end
   
   
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
   end
+  
   
   def apply_discount
     #binding.pry
