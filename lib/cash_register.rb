e79a37fd1f7940b89d80d62d2d36195c8719c498
Carl J <carlj1881@yahoo.com>
2020-10-02 04:10:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   #    last_transaction
   #binding.pry
     #@price_list << price
-    @last_transaction = price * quantity?
+    @last_transaction = price * quantity
     quantity.times do
     items << title
     end
