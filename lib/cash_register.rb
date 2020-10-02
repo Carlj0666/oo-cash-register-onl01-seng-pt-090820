3691d3508c1d55c15e1619eae6aae00a141fbdfa
Carl J <carlj1881@yahoo.com>
2020-10-02 00:16:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require"pry"
 
+class CashRegister
+  
+  attr_accessor :total, :discount, :quantity
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    #@quantity = quantity
+  end
+  
+  
+  def add_item(title, price, quantity = 1)
+          #binding.pry
+          @total = price * quantity
+          #figure out how to access the cash register's instance total with what we have
+  # @total += price
+  # @total * quantity
+
+  end
+  
+  
+end
