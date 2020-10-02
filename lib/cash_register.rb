8b3192891a141e1dca02c9b6fa0dc1a0c8197418
Carl J <carlj1881@yahoo.com>
2020-10-02 00:24:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     
     self.@total = price * quantity
-          binding.pry
+          #binding.pry
           #figure out how to access the cash register's instance total with what we have
    receipt
 
