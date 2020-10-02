b8335b1870c0684e8ae36ca697b907b469a36fb6
Carl J <carlj1881@yahoo.com>
2020-10-02 01:24:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,6 @@ class CashRegister
   def apply_discount
     #binding.pry
     self.total -= self.discount * 10
-    
     return "After the discount, the total comes to $#{self.total}."
     
   end
